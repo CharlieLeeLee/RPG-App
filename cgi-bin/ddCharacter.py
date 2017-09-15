82224890e488fb0e334a1fa10c5a4a453a4308d5
Charlie Norvell <charlie.norvell@gmail.com>
2017-09-15 00:58:14 -0400

    Continuing D&D Character class
    Adding initial race math and benefits.
    Finished: Dwarf, Dragonborn, Elf, Gnome
    Need to do: Half-elf, Half-orc, Human, Halfling, and Tiefling
    Thankfully, most of these don't have subraces to worry about.

--- cgi-bin/ddCharacter.py
+++ cgi-bin/ddCharacter.py
@@ -14,12 +14,15 @@ class DnDCharacter:
 
 	def __init__(self, name):
 		self.name = name
-		self.abilities = [] # a blank array for ability scores
-		self.feats = []		# a blank array for features
-		self.languages = []	# language proficiencies
-		self.weapons = []	# weapon proficiencies
-		self.armor = []		# armor proficiencies
-		self.tools = []		# tool proficiencies
+		self.abilities = [] 		# a blank array for ability scores
+		self.feats = []				# a blank array for features
+		self.languages = []			# language proficiencies
+		self.weapons = []			# weapon proficiencies
+		self.armor = []				# armor proficiencies
+		self.tools = []				# tool proficiencies
+		self.skills = [(0,0)]*18	# list of 18 tuples for skills the first number is the
+									#	skill mod, while the second is whether or not the 
+									#	character has proficiency
 		self.HP = 0
 		self.level = 1
 		
@@ -79,7 +82,7 @@ class DnDCharacter:
 				self.abilities[ability] = helper
 	
 	
-	# choose race, which affects ability scores
+	# choose race, which affects ability scores, languages, proficiencies, and feats
 	def chooseRace(race, subrace = None):
 		self.race = race
 		if race == "DRAGONBORN":
@@ -98,6 +101,7 @@ class DnDCharacter:
 			self.feats.append("Darkvision 60ft.")
 			self.feats.append("Dwarven Resilience", "Stone Cunning")
 			self.weapons.append("Battleaxe", "handaxe", "light hammer", "warhammer")
+			# addProficiency() choose smith's tools, brewer's supplies, or mason's tools
 			self.languages.append("Common", "Dwarvish")
 			if subrace == "HILL DWARF":
 				self.abilities[4] += 1
@@ -105,3 +109,38 @@ class DnDCharacter:
 			elif subrace = "MOUNTAIN DWARF":
 				self.abilities[0] += 2
 				self.armor.append("light", "medium")
+		elif race == "ELF":
+			self.abilities[1] += 2
+			self.size = "Medium"
+			self.speed = 30
+			self.feats.append("Darkvision 60ft", "Fey Ancestry", "Trance")
+			self.skills[11] = (0, 1)
+			self.languages.append("Common", "Elvish")
+			if subrace == "HIGH ELF":
+				self.abilities[3] +=1
+				self.weapons.append("longsword","shortsword","shortbow","longbow")
+				#addSpell() one cantrip from the wizard spell list, INT is spellcasting ability
+				#addLanguage() one extra language
+			elif subrace == "WOOD ELF":
+				self.abilities[4] +=1
+				self.weapons.append("longsword","shortsword","shortbow","longbow")
+				self.speed = 35
+				self.feats.append("Mask of the Wild")
+			elif subrace == "DROW":
+				self.abilities[5] += 1
+				self.feats.append("Darkvision 120ft")
+				self.weapons.append("rapiers", "shortsword", "hand crossbow")
+				self.feats.append("Sunlight Sensitivity", "Drow Magic")
+		elif race == "GNOME":
+			self.abilities[3] += 2
+			self.size = "Small"
+			self.speed = 25
+			self.feats.append("Darkvision 60ft", "Gnome Cunning")
+			self.languages.append("Common", "Gnomish")
+			if subrace == "ROCK GNOME":
+				self.abilities[2] +=1
+				self.feats.append("Artificer's Lore", "Tinker")
+			elif subrace == "FOREST GNOME":
+				self.abilities[1] += 1
+				#addSpell() minor illusion cantrip
+				self.feats.append("Speak with Small Beasts")
